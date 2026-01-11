2f604256e301e0a7844d66cc79f52670bf3a8dd5
Divyanshu-hash <rishugiri056@gmail.com>
2026-01-11 23:39:54 +0530

    Dynamic Programing - add climbing Stairs problem

---
+++ Dyanamic_Programing/climbingStrairs.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int solve(int n, vector<int>& dp) {
+    if (n == 0 || n == 1)
+        return 1;
+
+    if (dp[n] != -1)
+        return dp[n];
+
+    return dp[n] = solve(n - 1, dp) + solve(n - 2, dp);
+}
+
+int climbStairs(int n) {
+    vector<int> dp(n + 1, -1);
+    return solve(n, dp);
+}
+
+int main() {
+    int n = 5;
+    cout << "Ways: " << climbStairs(n) << endl;
+    return 0;
+}
