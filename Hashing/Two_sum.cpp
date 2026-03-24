e65e5d7cdbb4154487608ceb447e45d65e22f182
Divyanshu-hash <rishugiri056@gmail.com>
2026-03-24 23:27:31 +0530

    add Two Sum Problem

---
+++ Hashing/Two_sum.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n, target;
+    cin >> n;
+
+    vector<int> nums(n);
+    for(int i = 0; i < n; i++) {
+        cin >> nums[i];
+    }
+
+    cin >> target;
+
+    unordered_map<int, int> mp; // value -> index
+
+    for(int i = 0; i < n; i++) {
+        int complement = target - nums[i];
+
+        if(mp.find(complement) != mp.end()) {
+            cout << mp[complement] << " " << i << endl;
+            return 0;
+        }
+
+        mp[nums[i]] = i;
+    }
+
+    cout << "No solution found" << endl;
+    return 0;
+}
